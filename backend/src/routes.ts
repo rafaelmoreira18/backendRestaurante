c53f10d59344194810616e9633286492f2292ac4
rafaelmoreira18 <rafafelipe9444@gmail.com>
2025-07-21 23:51:11 +0200

    first commit changes

---
+++ backend/src/routes.ts
@@ -0,0 +1,10 @@
+import e, {Router} from 'express';
+import { CreateUserController } from './controllers/user/CreateUserController';
+
+
+const router = Router();
+
+
+router.post('/users', new CreateUserController().handle)
+
+export{router};
