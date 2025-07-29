27c95fa4cab220d93cb42122450702c24e8a78a5
rafaelmoreira18 <rafafelipe9444@gmail.com>
2025-07-29 03:44:14 +0200

    commit changes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,16 @@
-import e, {Router} from 'express';
+import {Router} from 'express';
 import { CreateUserController } from './controllers/user/CreateUserController';
-
+import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailUserController } from './controllers/user/DetailUserController';
+import {isAuthenticated} from './middlewares/isAuthenticated'
 
 const router = Router();
 
 
 router.post('/users', new CreateUserController().handle)
+
+router.post('/session', new AuthUserController().handle)
+
+router.get('/me',isAuthenticated, new DetailUserController().handle)
 
 export{router};
