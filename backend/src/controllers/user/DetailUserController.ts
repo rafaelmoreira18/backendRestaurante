9c25b9f3832e02852648b514216110d62647d23a
rafaelmoreira18 <rafafelipe9444@gmail.com>
2025-08-01 04:57:34 +0200

    json updates installing dependecies

--- backend/src/controllers/user/DetailUserController.ts
+++ backend/src/controllers/user/DetailUserController.ts
@@ -6,11 +6,9 @@ class DetailUserController{
 
         const user_id = req.user_id;
 
-        console.log("ID DO USER: ", user_id)
-
         const detailUserService = new DetailUserService();
 
-        const user = await detailUserService.execute();
+        const user = await detailUserService.execute(user_id);
 
         return res.json(user);
     }
